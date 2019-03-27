604a623bd9b3e6a0a1bf63f0eb8bd2056a15ee72
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-27 04:18:24 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog 
+  
+  #This method takes in an argument of a dog's name and sets the argument equal to a variable, this_dogs_name.
+  def name=(dog_name) #name= is a method, there is no space between the name and equals
+    this_dogs_name = dog_name
+  end
+  
+  #This method is responsible for reporting or reading the name 
+  def name 
+    this_dogs_name
+  end 
+end
+
+#Our two methods therefore are responsible for "setting" and "getting" an individual dog's name.
+
+lassie = Dog.new 
+lassie.name = "Lassie"
+
+lassie.name #=> "Lassie"
+
