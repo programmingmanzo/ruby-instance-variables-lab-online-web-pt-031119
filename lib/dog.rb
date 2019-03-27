754ad85e1fb75c144cc4d13f65113d8d68438c2f
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-27 04:24:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  
+  #This method takes in an argument of a dog's name and sets the argument equal to a variable, this_dogs_name.
+  
+  def name=(dog_name) #name= is a method, there is no space between the name and equals
+    @this_dogs_name = dog_name 
+  end
+  
+  #This method is responsible for reporting or reading the name. 
+  
+  def name 
+    @this_dogs_name #Placing the @ sign infront of the variable makes a variable accessible in any instance method in a particular instance of a class.  
+  end 
+end
+
+#Our two methods therefore are responsible for "setting" and "getting" an individual dog's name.
+
+lassie = Dog.new 
+lassie.name = "Lassie"
+
+lassie.name #=> "Lassie"
+
